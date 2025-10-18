6913e1f03863f254440ef346502ab03cb6af5700
mkielak <kielakowsky17@gmail.com>
2025-10-18 17:37:25 +0200

    Added base for editor and basic styling

---
+++ src/components/Editor.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Editor = () => {
+  return (
+    <div className="editor-container">
+        <label htmlFor="editor">Editor</label>
+        <textarea id="editor"></textarea>
+    </div>
+  )
+}
+
+export default Editor
