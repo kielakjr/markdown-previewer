3e5bd0b36bed5980dd356a8161507f4b21282586
mkielak <kielakowsky17@gmail.com>
2025-10-18 18:58:32 +0200

    Added dompurify for safety

--- src/components/Previewer.jsx
+++ src/components/Previewer.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { marked } from 'marked'
+import DOMPurify from 'dompurify';
 
 const Previewer = ({input}) => {
+
+  const createMarkup = (text) => {
+    const clean = DOMPurify.sanitize(marked.parse(text));
+    return { __html: clean };
+  };
+
   return (
     <div className="previewer-container">
         <label htmlFor="preview">Previewer</label>
-        <div id="preview" dangerouslySetInnerHTML={{ __html: marked.parse(input) }}></div>
+        <div id="preview" dangerouslySetInnerHTML={createMarkup(input)}></div>
     </div>
   )
 }
